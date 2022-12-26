3218ef9ee53fd4ba84b94f5188fb84aacce4861f
calvinAE <58550080+calvinAE@users.noreply.github.com>
2022-12-26 13:12:29 +0100

    Project init

---
+++ webapp/src/main/java/ehb/be/webapp/WebappApplication.java
@@ -0,0 +1,14 @@
+package ehb.be.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication
+public class WebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebappApplication.class, args);
+    }
+
+}
