4c86cb1859c48bdb3863a0fd0aecc2763b22aed9
calvinAE <58550080+calvinAE@users.noreply.github.com>
2023-01-05 18:31:26 +0100

    Added Security Configuration

---
+++ webapp/src/main/java/ehb/be/webapp/security/CustomSecurityConfig.java
@@ -0,0 +1,49 @@
+package ehb.be.webapp.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class CustomSecurityConfig {
+
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/products","/categories","/register","/registration").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                        .defaultSuccessUrl("/",true)
+                )
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout","GET"));
+
+        return http.build();
+    }
+
+
+    @Bean
+    @Primary
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+
+
+
+}
+
+
