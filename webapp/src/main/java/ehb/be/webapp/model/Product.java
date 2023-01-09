d59c82f451d634d13f512cab07fa2b4fb8929811
calvinAE <58550080+calvinAE@users.noreply.github.com>
2023-01-09 23:02:06 +0100

    Added lombok

--- webapp/src/main/java/ehb/be/webapp/model/Product.java
+++ webapp/src/main/java/ehb/be/webapp/model/Product.java
@@ -22,53 +22,6 @@ public class Product {
     private String description;
     private double price;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
 
     public Product(String name, Category category, String description, double price) {
         this.name = name;
