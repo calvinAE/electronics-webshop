6cc9dfc5287c227cee79b7c8b9b542fc4c0c9df2
calvinAE <58550080+calvinAE@users.noreply.github.com>
2023-01-09 20:11:36 +0100

    Added remove user

--- webapp/src/main/java/ehb/be/webapp/controller/UserController.java
+++ webapp/src/main/java/ehb/be/webapp/controller/UserController.java
@@ -6,13 +6,12 @@ import ehb.be.webapp.model.User;
 import ehb.be.webapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
@@ -22,17 +21,23 @@ public class UserController {
     public Iterable<User> getAll(){ return userRepository.findAll(); }
 
 
-    @GetMapping("/users")
+    @GetMapping("")
     public String index(){
 
       return "/users/index";
     }
 
-    @GetMapping("/users/makeAdmin")
+    @GetMapping("/makeAdmin")
     public String makeAdmin(Long id){
        Optional<User> user =  userRepository.findById(id);
         user.get().setRole("Admin");
         userRepository.save(user.get());
+        return "redirect:/users";
+    }
+
+    @RequestMapping(value = "/delete/{id}",method = RequestMethod.GET)
+    public String deleteUser(@PathVariable(value = "id")Long id){
+        userRepository.deleteById(id);
         return "redirect:/users";
     }
 
